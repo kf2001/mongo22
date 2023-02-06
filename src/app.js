f2a087db1b46087cc63d0ae503aaf5fabb203bda
kf2001 <48378596+kf2001@users.noreply.github.com>
2023-02-06 23:22:46 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,15 +12,63 @@ const express = require('@feathersjs/express');
 const socketio = require('@feathersjs/socketio');
 var app=express();
 var server = require('http').createServer(app);
+var allClients=[];
 
 var io = require('socket.io').listen(server);
-
 io.sockets.on('connection', function (socket) {
-  
-  console.log("conn");
-}
-              );
 
+  allClients.push(socket);
+
+  if (activeClients < 20) {
+
+    activeClients += 1;
+    console.log(activeClients);
+    io.sockets.emit('message', { clients: activeClients });
+    socket.emit('numero', activeClients % 2 + 1);
+
+
+    socket.on('disconnect', function () {
+      activeClients -= 1;
+      var i = allClients.indexOf(socket);
+
+      if (activeClients % 2 == 1) attende = allClients[i].avversario; else attende = null;
+      allClients[i].avversario.emit('andato');
+      io.sockets.emit('message', { clients: activeClients });
+      delete allClients[i];
+
+
+    });
+
+    socket.on('daclient', function (msg) {
+      console.log("ricevuta");
+    
+      socket.emit('draw', msg);
+      socket.avversario.emit('draw', msg);
+
+    });
+    socket.on('join', function (msg) {
+      console.log("nick arrivato: " + msg);
+      socket.nickname = msg;
+      nicks += msg + "-";
+      console.log("connesso " + msg);
+
+      io.sockets.emit('lista', nicks);
+
+      if (activeClients % 2 == 1) {// cerca avversario 
+        attende = socket;
+     
+      }
+      if (activeClients % 2 == 0) {// trovato avversario 
+        socket.avversario = attende;
+        attende.avversario = socket;
+
+        socket.emit('start', socket.avversario.nickname, 1);
+        socket.avversario.emit('start', socket.nickname, 2);
+
+      }
+    });
+  }
+});
 /*
 const middleware = require('./middleware');
 const services = require('./services');
