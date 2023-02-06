b9c0ade49cd1863c7f14a6aae4f174ff517bef8a
kf2001 <48378596+kf2001@users.noreply.github.com>
2023-02-06 23:35:32 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -69,6 +69,9 @@ io.sockets.on('connection', function (socket) {
     });
   }
 });
+
+module.exports.createApp = function createApp() {}
+module.exports.startApp = function startApp() {}
 /*
 const middleware = require('./middleware');
 const services = require('./services');
